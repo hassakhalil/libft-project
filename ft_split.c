44ca72a0e1b375df18439ead02a0f75a802bfe0d
hassakhalil <hkhalil@student.1337.ma>
2021-08-13 22:20:26 +0100

    ft_split almost done

--- ft_split.c
+++ ft_split.c
@@ -24,15 +24,15 @@ int	number_of_string(char const *s, char c)
 	return (n);
 }
 
-int	*string_size_table(int number_of_string)
+int	*string_size_table(int number_of_string, char const *s, char c)
 {
 	int	i;
 	int	j;
 	int	k;
 	int	*n;
 
-	n = malloc((number_of_string(s, c) + 1) * 4);
-	if (n = 0)
+	n = malloc((number_of_string + 1) * 4);
+	if (n == 0)
 		return (0);
 	i = 0;
 	k = 0;
@@ -46,18 +46,13 @@ int	*string_size_table(int number_of_string)
 		}
 		if (j != 0)
 		{
-			n[k] = malloc(j + 1);
-			if (n[k] == 0)
-				return (0);
 			n[k] = j + 1;
 			k++;
 		}
 		i++;
 	}
-	n[k] = malloc(4);
-	if (n[k] = 0)
-		return (0);
-	n[k] = 0;
+	if (j != 0)
+		n[k] = 0;
 	return (n);
 }
 
@@ -65,16 +60,51 @@ char	**ft_split(char const *s, char c)
 {
 	char	**x;
 	int	*n;
-	int	total;
 	int	i;
+	int	j;
+	int	k;
 
-	n = string_size_table(number_of_string(s, c));
+	n = string_size_table(number_of_string(s, c), s, c);
 	if (n == 0)
 		return (0);
+	i = 0;
+	j = 0;
 	while (n[i] != 0)
 	{
-		
+		j = n[i] + j;
+		i++;
 	}
+	x = malloc(j + sizeof(NULL));
+	if (x == 0)
+		return (0);
+	i = 0;
+	while (n[i] != 0)
+	{
+		x[i] = malloc(n[i]);
+		if (x[i] == 0)
+			return (0);
+		i++;
+	}
+	i = 0;
+	k = 0;
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (s[i] != c && s[i] != '\0')
+		{
+			x[k][j] = s[i];
+			j++;
+			i++;
+		}
+		if (j != 0)
+		{
+			x[k][j] = '\0';
+			k++;
+		}
+		i++;
+	}
+	x[k] = NULL;
+	free (n);
 	return (x);
 }
 
@@ -82,7 +112,19 @@ char	**ft_split(char const *s, char c)
 int main ()
 {
 	char	**x;
-	
-	x = ft_split("hello----world-", '-');
-	printf("%s\n", x[0]);
+	int	i = 0;
+	x = ft_split("wetrtwerweroooooooootwtwerotweter", '\0');
+	while (x[i] != NULL)
+	{
+		printf("[%s]\n", x[i]);
+		i++;
+	}
+	i = 0;
+	while (x[i] != NULL)
+	{
+		free (x[i]);
+		i++;
+	}
+	free (x[i]);
+	free (x);
 }
