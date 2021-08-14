a0664460bcf9915d4a82227f315025af25a93206
hassakhalil <hkhalil@student.1337.ma>
2021-08-14 20:23:46 +0100

    ft_split

--- ft_split.c
+++ ft_split.c
@@ -1,5 +1,40 @@
 #include <stdlib.h>
-#include <stdio.h>
+#include <string.h>
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	if (dest == 0 || src == 0)
+		return (0);
+	while (i < n)
+	{
+		((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
+		i++;
+	}
+	return (dest);
+}
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	int	i;
+
+	if (dest == 0 || src == 0)
+		return (0);
+	if (src < dest)
+	{
+		i = n - 1;
+		while (0 <= i)
+		{
+			((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
+			i--;
+		}
+	}
+	else
+		ft_memcpy(dest, src, n);
+	return ((void *)dest);
+}
 
 int	number_of_strings(char const *s, char c)
 {
@@ -26,25 +61,23 @@ int	number_of_strings(char const *s, char c)
 	}
 	return (n);
 }
-	
+
 char	**ft_split(char const *s, char c)
 {
 	char	**x;
 	 int	i;
 	 int	j;
 	 int	k;
-	 int	l;
 	 int	n;
 
 	n = number_of_strings(s, c);
+	i = 0;
+	k = 0;
 	if (n == 0)
 		return (0);
 	x = malloc((n + 1) * sizeof(char *));
-	if (!s || !c ||!x)
+	if (!s || !c || !x)
 		return (0);
-	i = 0;
-	j = 0;
-	k = 0;
 	while (n > 0)
 	{
 		j = 0;
@@ -55,19 +88,12 @@ char	**ft_split(char const *s, char c)
 		x[k] = malloc(j + 1);
 		if (x[k] == 0)
 			return (0);
-		l = 0;
-		while (i < i + j)
+		if (ft_memmove(x[k], &s[i], j))
 		{
-			x[k][l] = s[i];
-			i++;
-			l++;
-			j--;
-		}
-		if (l != 0)
-		{
-			x[k][l] = 0;
+			x[k][j] = 0;
 			k++;
 			n--;
+			i = i + j;
 		}
 	}
 	x[k] = NULL;
@@ -79,7 +105,8 @@ int main ()
 {
 	char	**x;
 	int	i = 0;
-	x = ft_split(NULL, '=');
+	x = ft_split("1222==hello==there====how=are=youuuu=16", 'e');
+//	x = ft_split(NULL, 'a');
 	if (x == 0)
 		return (0);
 	while (x[i] != NULL)
