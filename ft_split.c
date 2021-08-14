838e2c99f833955f89d57d9e2beb40a308adf4e8
hassakhalil <hkhalil@student.1337.ma>
2021-08-14 22:08:37 +0100

    ft_split done

--- ft_split.c
+++ ft_split.c
@@ -46,48 +46,40 @@ int	number_of_strings(char const *s, char c)
 	n = 0;
 	if (!s || !c)
 		return (0);
-	while (s[i] != 0)
+	while (s[i])
 	{
 		j = 0;
-		while (s[i] != c && s[i] != 0)
+		while (s[i] != c && s[i])
 		{
 			j = 1;
 			i++;
 		}
 		if (j == 1)
 			n++;
-		if (s[i] != 0)
+		if (s[i])
 			i++;
 	}
 	return (n);
 }
 
-char	**ft_split(char const *s, char c)
+void	ft_split_1(char const *s, char c, char **x, int n)
 {
-	char	**x;
-	 int	i;
-	 int	j;
-	 int	k;
-	 int	n;
+	int	i;
+	int	j;
+	int	k;
 
-	n = number_of_strings(s, c);
 	i = 0;
 	k = 0;
-	if (n == 0)
-		return (0);
-	x = malloc((n + 1) * sizeof(char *));
-	if (!s || !c || !x)
-		return (0);
 	while (n > 0)
 	{
 		j = 0;
 		while (s[i] == c)
 			i++;
-		while (s[i + j] != c && s[i + j] != 0)
+		while (s[i + j] != c && s[i + j])
 			j++;
 		x[k] = malloc(j + 1);
-		if (x[k] == 0)
-			return (0);
+		if (!x)
+			return ;
 		if (ft_memmove(x[k], &s[i], j))
 		{
 			x[k][j] = 0;
@@ -97,6 +89,20 @@ char	**ft_split(char const *s, char c)
 		}
 	}
 	x[k] = NULL;
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**x;
+	 int	n;
+
+	n = number_of_strings(s, c);
+	if (n == 0)
+		return (0);
+	x = malloc((n + 1) * sizeof(char *));
+	if (!s || !c || !x)
+		return (0);
+	ft_split_1(s, c, x, n);
 	return (x);
 }
 
@@ -105,8 +111,8 @@ int main ()
 {
 	char	**x;
 	int	i = 0;
-	x = ft_split("1222==hello==there====how=are=youuuu=16", 'e');
-//	x = ft_split(NULL, 'a');
+	x = ft_split("1222==hello==there====how=are=youuuu=16", '=');
+	//x = ft_split(NULL, 'a');
 	if (x == 0)
 		return (0);
 	while (x[i] != NULL)
